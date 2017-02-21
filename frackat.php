323d8ce24fedc0b53a39df376c5246ee04c68084
grafdadofganja <dadofganja@mail.ru>
2017-02-21 19:05:22 +0500

    correcting
    fraqcs

--- frackat.php
+++ frackat.php
@@ -10,6 +10,78 @@
 				</figure>		
 		 	</a>
 		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=gays">
+			 	<figure>
+					<img src="./img/fracs/gays.jpg" alt="">
+					<figcaption>Черные геи</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=amazons">
+			 	<figure>
+					<img src="./img/fracs/amazons.jpg" alt="">
+					<figcaption>Амазонки Курцов</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=suillae">
+			 	<figure>
+					<img src="./img/fracs/suillae.jpg" alt="">
+					<figcaption>Маслята</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=americans">
+			 	<figure>
+					<img src="./img/fracs/americans.jpg" alt="">
+					<figcaption>Индейцы</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=adepts">
+			 	<figure>
+					<img src="./img/fracs/adepts.jpg" alt="">
+					<figcaption>Адепты</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=adepts">
+			 	<figure>
+					<img src="./img/fracs/adepts.jpg" alt="">
+					<figcaption>Адепты</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=adepts">
+			 	<figure>
+					<img src="./img/fracs/adepts.jpg" alt="">
+					<figcaption>Адепты</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=adepts">
+			 	<figure>
+					<img src="./img/fracs/adepts.jpg" alt="">
+					<figcaption>Адепты</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
+		 <div class="perslink">
+		 	<a href="./category.php?c=fracs&p=adepts">
+			 	<figure>
+					<img src="./img/fracs/adepts.jpg" alt="">
+					<figcaption>Адепты</figcaption>
+				</figure>		
+		 	</a>
+		 </div>	
 		 
 
 	</main>
