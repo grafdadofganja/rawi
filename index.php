ea795778149be70341c609391f16efcf1ef57dc6
grafdadofganja <dadofganja@mail.ru>
2017-03-06 14:43:01 +0500

    rhththtyh
    thththth

--- index.php
+++ index.php
@@ -3,13 +3,47 @@
 ?>
 
 <main>
-	<img src="./img/crimea.jpg" class="acrimea"/>
+	
+	<td>
+	<!--img src="./img/crimea.jpg" class="acrimea"/>
 	<img src="./img/kurti.jpg" class="acrimea"/>
 	<img src="./img/belt.jpg" class="acrimea"/>
 	<img src="./img/daru.jpg" class="acrimea"/>
-	<img src="./img/dnipro.jpg" class="acrimea"/>
+	<img src="./img/dnipro.jpg" class="acrimea"/-->
+	    <a href='#'><img src="./img/crimea.jpg" class="acrimea" /></a>
+
+	</td>
+
 </main>
 
 <?php
 	include("./template/footer.php");
 ?>
+<script type="text/javascript">
+
+	$(document).ready(function () {
+
+	    $('td a').click(function () {
+
+	        var o=$(this).parent();
+
+	        var url=o.find('img').attr('src');
+
+	        var html='<a href="#" class="clone"><img src="'+url+'" /></a>'
+
+	        o.append(html);
+	        o=o.find('.clone');
+
+	        o.animate({width:'150%',height:'150%'});
+
+	        o.click(function () {
+
+	            $(this).remove();
+
+	        });
+
+	    });
+
+	});
+
+	</script>
