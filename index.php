e2d9ef06a062f0947caa3648ccd32f5be8e9a677
grafdadofganja <dadofganja@mail.ru>
2017-03-07 20:35:44 +0500

    delete shit
    css etc

--- index.php
+++ index.php
@@ -19,31 +19,4 @@
 <?php
 	include("./template/footer.php");
 ?>
-<script type="text/javascript">
 
-	$(document).ready(function () {
-
-	    $('td a').click(function () {
-
-	        var o=$(this).parent();
-
-	        var url=o.find('img').attr('src');
-
-	        var html='<a href="#" class="clone"><img src="'+url+'" /></a>'
-
-	        o.append(html);
-	        o=o.find('.clone');
-
-	        o.animate({width:'150%',height:'150%'});
-
-	        o.click(function () {
-
-	            $(this).remove();
-
-	        });
-
-	    });
-
-	});
-
-	</script>
