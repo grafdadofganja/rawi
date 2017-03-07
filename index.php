00e98682f6f65116fcf09d7afdd3485fdffabc7d
zlapot <zloi.apot@yandex.ru>
2017-03-07 17:49:11 +0300

    Add modal windows for image

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 	include("./template/header.php");
 ?>
 
-<main>
-	<img src="./img/crimea.jpg" class="acrimea"/>
-	<img src="./img/kurti.jpg" class="acrimea"/>
-	<img src="./img/belt.jpg" class="acrimea"/>
-	<img src="./img/daru.jpg" class="acrimea"/>
-	<img src="./img/dnipro.jpg" class="acrimea"/>
+<main class="main-page">
+	<img src="./img/crimea.jpg" class="map-img"/>
+	<img src="./img/kurti.jpg" class="map-img"/>
+	<img src="./img/belt.jpg" class="map-img"/>
+	<img src="./img/daru.jpg" class="map-img"/>
+	<img src="./img/dnipro.jpg" class="map-img"/>
 </main>
 
 <?php
